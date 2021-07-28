fb2ca2dada73b7a3a553669516c37c9cdb3ce66a
Anastasiia Tymoshchuk <atymoshchuk@icloud.com>
2021-07-28 06:40:07 +0200

    Format the code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,17 +21,17 @@ from pathlib import Path
 current_dir = Path(__file__).parent.absolute()
 code_dir = current_dir.parents[1] / "app"
 
-sys.path.insert(0, os.path.abspath('../../app'))
+sys.path.insert(0, os.path.abspath("../../app"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'SimpleDocTutor'
-copyright = '2020, Anastasiia Tymoshchuk'
-author = 'Anastasiia Tymoshchuk'
+project = "SimpleDocTutor"
+copyright = "2020, Anastasiia Tymoshchuk"
+author = "Anastasiia Tymoshchuk"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = "0.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -48,7 +48,7 @@ extensions = [
 source_suffix = [".rst", ".md"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -61,12 +61,12 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 def run_apidoc(_):
